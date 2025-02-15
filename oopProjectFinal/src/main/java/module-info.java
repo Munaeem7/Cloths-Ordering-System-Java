1b7c6167dcc6bc160e56066dd37b556b2fbd85cf
Munaeem <160645028+Munaeem7@users.noreply.github.com>
2025-02-15 23:51:00 +0500

    Delete oopProjectFinal directory

--- oopProjectFinal/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.oopproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.compiler;
-
-
-    opens com.example.oopproject to javafx.fxml;
-    exports com.example.oopproject;
-}
